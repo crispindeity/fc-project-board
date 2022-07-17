c62d1f1e4caa76d154191eaee1f03d33d0811fe4
HeechulShin <shoy1415@gmail.com>
2022-07-17 18:04:58 +0900

    게시글 뷰 테스트 내용 업데이트 - #16
    Description
    - `gradle build` 가 실패하여 실패 테스트를 예외 처리

---
+++ fc-project-board/src/main/java/fc/projectboard/controller/ArticleController.java
@@ -0,0 +1,11 @@
+package fc.projectboard.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@RequestMapping("/article")
+@Controller
+public class ArticleController {
+
+
+}
