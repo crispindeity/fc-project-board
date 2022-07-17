6ef911390d79b5d36796143509c7617a94722082
HeechulShin <shoy1415@gmail.com>
2022-07-18 01:47:02 +0900

    게시글 뷰 기본 골격 작성 - #16
    Description
    - 컨트롤러, 뷰, 테스트 완성

--- fc-project-board/src/test/java/fc/projectboard/controller/ArticleControllerTest.java
+++ fc-project-board/src/test/java/fc/projectboard/controller/ArticleControllerTest.java
@@ -38,7 +38,6 @@ class ArticleControllerTest {
                 .andExpect(model().attributeExists("articles"));
     }
 
-    @Disabled("구현 중")
     @DisplayName("[View][GET] 게시글 상세 페이지 - 정상 호출")
     @Test
     void givenNoting_whenRequestingArticleView_thenReturnArticleView() throws Exception {
@@ -49,7 +48,7 @@ class ArticleControllerTest {
                 .andExpect(status().isOk())
                 .andExpect(content().contentTypeCompatibleWith(MediaType.TEXT_HTML))
                 .andExpect(view().name("articles/detail"))
-                .andExpect(model().attributeExists("articles"))
+                .andExpect(model().attributeExists("article"))
                 .andExpect(model().attributeExists("articleComments"));
     }
 
