bf7bf79933cff469f3443d6930d2364c63d8536f
HeechulShin <shoy1415@gmail.com>
2022-07-17 20:11:43 +0900

    게시판 뷰 기본 골격 작성 - #16
    Description
    - 테스트 통과 확인 및 뷰 기본 작업

--- fc-project-board/src/test/java/fc/projectboard/controller/ArticleControllerTest.java
+++ fc-project-board/src/test/java/fc/projectboard/controller/ArticleControllerTest.java
@@ -24,7 +24,7 @@ class ArticleControllerTest {
         this.mvc = mvc;
     }
 
-    @Disabled("구현 중")
+
     @DisplayName("[View][GET] 게시글 리스트 (게시판) 페이지 - 정상 호출")
     @Test
     void givenNoting_whenRequestingArticlesView_thenReturnArticlesView() throws Exception {
@@ -33,7 +33,7 @@ class ArticleControllerTest {
         //when & then
         mvc.perform(get("/articles"))
                 .andExpect(status().isOk())
-                .andExpect(content().contentType(MediaType.TEXT_HTML))
+                .andExpect(content().contentTypeCompatibleWith(MediaType.TEXT_HTML))
                 .andExpect(view().name("articles/index"))
                 .andExpect(model().attributeExists("articles"));
     }
