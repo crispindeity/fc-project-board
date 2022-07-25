fb4ece786f535c9500311f74bf737e73b0edf14e
geombong <shoy1415@gmail.com>
2022-07-26 03:09:55 +0900

    게시글 뷰 템플릿 구성 - #21

--- fc-project-board/src/main/java/fc/projectboard/dto/ArticleWithCommentsResponse.java
+++ fc-project-board/src/main/java/fc/projectboard/dto/ArticleWithCommentsResponse.java
@@ -13,17 +13,17 @@ public class ArticleWithCommentsResponse {
     private String title;
     private String content;
     private String hashtag;
-    private LocalDateTime createAt;
+    private LocalDateTime createdAt;
     private String email;
     private String nickname;
     private Set<ArticleCommentResponse> articleCommentsResponse;
 
-    private ArticleWithCommentsResponse(Long id, String title, String content, String hashtag, LocalDateTime createAt, String email, String nickname, Set<ArticleCommentResponse> articleCommentsResponse) {
+    private ArticleWithCommentsResponse(Long id, String title, String content, String hashtag, LocalDateTime createdAt, String email, String nickname, Set<ArticleCommentResponse> articleCommentsResponse) {
         this.id = id;
         this.title = title;
         this.content = content;
         this.hashtag = hashtag;
-        this.createAt = createAt;
+        this.createdAt = createdAt;
         this.email = email;
         this.nickname = nickname;
         this.articleCommentsResponse = articleCommentsResponse;
